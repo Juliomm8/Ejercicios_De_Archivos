580e1ff2a33c57f1dc9a84de2d47edc4d9fd94c3
Juliomm <juliom2004@hotmail.com>
2024-12-10 20:57:06 -0500

    Update 0.1
    Añadida lógica para manejo de archivos: comprobar existencia, agregar contenido o crear nuevo archivo
    
    - Se verifica si el archivo existe; si es así, se agrega una nueva línea.
    - Si el archivo no existe, se crea y se escribe contenido inicial.
    - Se implementó manejo de errores con mensajes descriptivos usando perror.
    - Mejora la robustez del programa para operaciones con archivos.

--- Ejercicio1/EJ1_Archivos.c
+++ Ejercicio1/EJ1_Archivos.c
@@ -1,12 +1,34 @@
-
-
 #include <stdio.h>
 
-int main(){
+int main() {
+    const char *filename = "D:\\archivos\\archivo.txt";
+    FILE *file;
+
+    file = fopen(filename, "r");
+    if (file != NULL) {
+        printf("El archivo '%s' ya existe.\n", filename);
+        fclose(file);
 
 
+        file = fopen(filename, "a");
+        if (file == NULL) {
+            perror("Error al abrir el archivo existente");
+            return 1;
+        }
+        fprintf(file, "Añadiendo una nueva línea al archivo existente.\n"); 
+        printf("Se ha añadido contenido al archivo '%s'.\n", filename);
+        fclose(file);
+    } else {
 
-
+        file = fopen(filename, "w"); 
+        if (file == NULL) {
+            perror("Error al crear el archivo");
+            return 1;
+        }
+        fprintf(file, "\n esto es la segunda linea");
+        printf("El archivo '%s' ha sido creado y contenido escrito.\n", filename);
+        fclose(file);
+    }
 
     return 0;
 }
